cacc4f88b9a471933a99646f0dbb2015acfafab6
hieunguyengre <hieunguyengre@users.noreply.github.com>
2026-03-28 19:10:52 +0700

    Change default map center from HCMC to Hanoi

--- public/app.js
+++ public/app.js
@@ -186,7 +186,8 @@
   }
 
   function initMap() {
-    map = L.map('map', { center: [10.8231, 106.6297], zoom: 14, zoomControl: true });
+    // Default location: Hanoi, Vietnam
+    map = L.map('map', { center: [21.0285, 105.8542], zoom: 14, zoomControl: true });
     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
       subdomains: 'abcd', maxZoom: 20
